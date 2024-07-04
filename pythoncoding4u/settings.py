5c2dea7d593c3afebfa70450c3f6eda303009a60
Ajay <ajayoneness123@gmail.com>
2024-07-04 19:25:08 +0530

    testmonial update

--- pythoncoding4u/settings.py
+++ pythoncoding4u/settings.py
@@ -26,6 +26,7 @@ INSTALLED_APPS = [
     'projects',
     'services',
     'blogs',
+    'testmonials',
 ]
 
 MIDDLEWARE = [
--- testmonials/models.py
+++ testmonials/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+
+
+class Testimonial(models.Model):
+    RATING_CHOICES = [
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+    ]
+
+    name = models.CharField(max_length=255)
+    designation = models.CharField(max_length=255)
+    feedback = models.TextField()
+    rating = models.PositiveSmallIntegerField(choices=RATING_CHOICES)
+
+    def __str__(self):
+        return f"{self.name} - {self.designation}"
