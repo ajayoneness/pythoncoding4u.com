df5f61c5569cfd12fc334c38278cfd09ac795c5f
Ajay Pandit <ajayoneness123@gmail.com>
2024-07-10 01:07:30 +0530

    blog content added

--- BlogApp/urls.py
+++ BlogApp/urls.py
@@ -3,7 +3,10 @@ from . import views
 
 
 urlpatterns = [
+    path('api/<slug:slug>/', views.get_blog, name='get_blog'),
+    path('blogapi/', views.get_all_blogs, name='blogapi'),
     path('', views.allBlog, name="blogs"),
     path('<slug:slug>/', views.singleblog, name='single_blog'),
+     
 ]
 
